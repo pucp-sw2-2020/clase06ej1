d5211b4ddb1eba1a06510084c7c5082e28ec0b34
Stuardo <stuardo.lucho@gmail.com>
2020-02-27 04:02:37 -0500

    proyecto base

---
+++ src/main/java/sw2/clase06ej1/Clase06ej1Application.java
@@ -0,0 +1,13 @@
+package sw2.clase06ej1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase06ej1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase06ej1Application.class, args);
+    }
+
+}
---
+++ src/test/java/sw2/clase06ej1/Clase06ej1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sw2.clase06ej1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase06ej1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
